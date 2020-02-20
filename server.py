3a8d736f96fea12dd8caf8bc0ffaa8fe56a676b9
Vishwakarma Raj <34263131+neon97@users.noreply.github.com>
2020-02-20 18:21:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import os
 import requests
 import datetime
 
+
 from flask import request
 from flask import make_response
 from flask import Flask
@@ -16,8 +17,14 @@ respone = ""
 
 seeds = [
     {
-        "text": "helloindia",
-        "params": "helloworld"
+        "fulfillmentText": "we dont have any hotels in mumbai"
+
+    }
+]
+
+leaches = [
+    {
+        "response": "Working fine"
     }
 ]
 
@@ -25,25 +32,54 @@ seeds = [
 @app.route("/trial")
 def home():
     print(respone)
-    leaches = [
-        {
-            "response": respone
-        }
-    ]
+
     return jsonify(leaches)
-
-
-
 
 
 @app.route("/check", methods=['POST'])
 def checker():
     req = request.get_json(silent=True, force=True)
-    global respone
-    respone = str(req["queryResult"]["fulfillmentText"])
-    print(respone)
-  
+    print(req)
     return jsonify(seeds)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json(silent=True, force=True)
+    # print(data)
+    print(data['queryResult']['queryText'])
+
+    # if data['queryResult']['fulfillmentText'] == 'At what location you gonna stay?':
+    #     reply = {
+    #         "messages": [
+    #             {
+    #                 "platform": "Web",
+    #                 "speech": "Text response",
+    #                 "type": 0
+    #             }
+    #         ]
+    #     }
+    #     print(reply)
+    #     res = reply
+
+    if data['queryResult']['queryText'] == 'mumbai':
+        res = get_data()
+        reply = make_response(res)
+        print(reply)
+
+    else:
+        res = leaches
+        # message = request.form["kolkatta"]
+        # print(message)
+    return jsonify(res)
+
+
+ def get_data():
+    speech = "we dont have any location at mumbai"
+
+    return{
+        "fulfillmentText": speech
+    }
 
 
 if __name__ == "__main__":
